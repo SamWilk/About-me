e30eff04c45adea5c6c4581cfd4b03d314921b21
Sam Wilk <82825157+SamWilk@users.noreply.github.com>
2023-04-07 13:48:45 -0400

    Missed something

--- src/components/github/Github.js
+++ src/components/github/Github.js
@@ -8,7 +8,7 @@ const Github = () => {
   //Call Api to get data
   useEffect(() => {
     async function getData() {
-      const res = await fetch("http://api.sam-wilk.com/githubprojects");
+      const res = await fetch("https://api.sam-wilk.com/githubprojects");
       const body = await res.json();
       setResponse(body);
     }
